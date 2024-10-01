6436e65d0d410595342bfbbde6860327e1db67a2
namycodes <namy.hacker0@gmail.com>
2024-10-01 02:04:06 +0200

    Finished All The Tasks

---
+++ 12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+    return {
+        allEmployees:{
+            ...employeesList,
+        },
+        getNumberOfDepartments(numberOfDepartments){
+            return Object.keys(numberOfDepartments).length
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+First Alx Backend Project by @namycodes
